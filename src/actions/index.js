45f8a687f64cb1cec5d7606f8853404788b65965
Ivan Fertikov <i-fertikov@marya.ru>
2017-05-23 17:39:17 +0400

    get API-key, begin Middleware, create action fetchWeather

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'b4cc37148964f351cc93dc50fc58b734';
+const ROOT_RUL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
