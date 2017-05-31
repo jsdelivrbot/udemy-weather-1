d05cb4b9bf23183136791cb5ee8dcc3c4a75be09
Ivan Fertikov <i-fertikov@marya.ru>
2017-05-31 17:42:50 +0400

    create Sparklines charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 
 class WeatherList extends Component {
 
 	renderWeather(cityData) {
+
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const humidity = cityData.list.map(weather => weather.main.humidity);
+		const pressure = cityData.list.map(weather => weather.main.pressure);
+
+		// console.log(temps);
+
 		return(
-			<tr key={cityData.city.id}>
-				<td>{cityData.city.name}</td>
+			<tr key={name}>
+				<td>{name}</td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
+
+				<td>
+					<Sparklines height={120} width={180} data={pressure}>
+						<SparklinesLine color="green" />
+					</Sparklines>
+				</td>
+
+				<td>
+					<Sparklines height={120} width={180} data={humidity}>
+						<SparklinesLine color="blue" />
+					</Sparklines>
+				</td>
 			</tr>
 		);
 	}
@@ -18,10 +44,10 @@ class WeatherList extends Component {
 			<table className="table table-hover">
 				<thead>
 				<tr>
-					<th>City</th>
-					<th>Temperature</th>
-					<th>Pressure</th>
-					<th>Humidity</th>
+					<th>Город</th>
+					<th>Температура</th>
+					<th>Давление</th>
+					<th>Влажность</th>
 				</tr>
 				</thead>
 
