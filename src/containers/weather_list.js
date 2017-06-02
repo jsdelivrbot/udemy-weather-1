0f88e766f6fb25022c9e573e64db4a97a74f766a
Ivan Fertikov <i-fertikov@marya.ru>
2017-06-02 17:45:43 +0400

    create google map

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
 
 class WeatherList extends Component {
 
@@ -11,11 +13,15 @@ class WeatherList extends Component {
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
 
+		const { lon, lat } = cityData.city.coord;
+		// const lon = cityData.city.coord.lon;
+		// const lat = cityData.city.coord.lat;
+
 		// console.log(temps);
 
 		return(
 			<tr key={name}>
-				<td>{name}</td>
+				<td><GoogleMap lat={lat} lon={lon} /></td>
 				<td>
 					<Chart data={temps} color="orange" units="K" />
 				</td>
